577cfb48d6f2734ca5e6d930bacc2e809b228d6f
Cas9Diego <DaCastro@outlook.com>
2022-08-10 14:18:50 -0500

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Diego Castro on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
